683637d315bd0bc5efc03b31fd54b2ed3ccb7e83
Daniel Dietrich <cafebab3@gmail.com>
2019-01-16 11:19:52 +0100

    Fixes the Travis-CI build

--- src/test/java/client/ConcurrentTest2.java
+++ src/test/java/client/ConcurrentTest2.java
@@ -21,6 +21,7 @@ import java.util.function.Predicate;
  * [INFO]    Compiling...
  * [INFO]       [WARN] JDT threw an exception: file:/home/ruslan/proj/javaslang-gwt/src/test/java/client/ConcurrentTestGwt.java: java.lang.ArrayIndexOutOfBoundsException: 1600
  */
+@SuppressWarnings("deprecation")
 public class ConcurrentTest2 extends GWTTestCase {
 
     @Override public String getModuleName()  {
